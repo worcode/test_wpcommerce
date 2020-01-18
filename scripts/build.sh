685db20f16c470dda0a20f08006b31e4918961f0
WorNut T <6457538+worcode@users.noreply.github.com>
2020-01-18 18:53:04 +0700

    add build script

---
+++ scripts/build.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+set -e
+set -p pipefail
+
+SCRIPT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SSL_PATH="${SCRIPT_PATH}/../ssl"
+
+function fail {
+    printf 'fail => %s' "$1" >&2
+    exit "${2-1}"
+}
+
+function createSelfCert {
+    command -v openssl || fail "openssl is not found, please install and try again !"
+
+    if ! [[ -d "${SSL_PATH}" ]]; then
+        mkdir "${SSL_PATH}"
+    fi;
+
+    sudo openssl req -x509 -nodes -days 365 -subj "/C=TH/ST=BKK/O=Any co, ltd./CN=localhost" -addext "subjectAltName=DNS:localhost" -newkey rsa:2048 -keyout "${SSL_PATH}/localhost.key" -out "${SSL_PATH}/localhost.crt"
+}
+
+function buildProxy {
+    command -v docker || fail "docker is not found !"
+
+    docker build -t wpapi-proxy:latest "${SCRIPT_PATH}/../nginx"
+}
+
+createSelfCert
+buildProxy
